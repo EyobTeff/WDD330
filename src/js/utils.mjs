ac0600d75422f387b5c588dabc41ef8b6c990c7f
EyobTeff <eyobteff@gmail.com>
2025-07-10 22:57:05 -0600

    first commit

---
+++ src/js/utils.mjs
@@ -0,0 +1,6 @@
+export function getParam(param) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get(param);
+  }
+  
